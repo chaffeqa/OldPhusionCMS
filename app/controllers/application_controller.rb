c2b70f6202941a0a92436011e9eed913245b2c6b
chaffeqa <chaffeqa@gmail.com>
2010-07-18 19:01:35 -0400

    quick fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,20 @@ class ApplicationController < ActionController::Base
   layout 'application'
   helper_method :get_home_node
 
-
   def get_home_node
+    if home_node?
+      return @home_node
+    end
+    return nil
+  end
+
+  def home_node?
     unless @home_node
       @home_node = Node.where(:menu_name => 'Home').first
       if @home_node.nil?
-        flash[:important_notice] = 'Home Page not detected, please create one.'
-        redirect_to(new_node_path(:home => "true"))
-        return false
+        raise ActiveRecord::RecordNotFound, "Home Page not detected, redirecting"
       end
     end
-    @home_node
   rescue ActiveRecord::RecordNotFound
     flash[:important_notice] = 'Home Page not detected, please create one.'
     redirect_to(new_node_path(:home => "true"))
